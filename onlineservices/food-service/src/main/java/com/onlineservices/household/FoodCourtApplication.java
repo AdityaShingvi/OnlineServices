78686b8062ea0aa6134ed1f3220663d76b886c88
AdityaS <adityashingvi13@gmail.com>
2019-07-22 00:16:33 -0700

    Initial project setup

---
+++ onlineservices/food-service/src/main/java/com/onlineservices/household/FoodCourtApplication.java
@@ -0,0 +1,29 @@
+package com.onlineservices.household;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class FoodCourtApplication extends Application<FoodCourtConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new Food CourtApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "Food Court";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<FoodCourtConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final Food CourtConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ onlineservices/food-service/src/main/java/com/onlineservices/household/FoodCourtConfiguration.java
@@ -0,0 +1,10 @@
+package com.onlineservices.household;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class FoodCourtConfiguration extends Configuration {
+    // TODO: implement service configuration
+}

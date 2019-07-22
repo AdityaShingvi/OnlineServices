315cfbec8d2664d369ca40641b1ca76c4cc74b1a
AdityaS <adityashingvi13@gmail.com>
2019-07-22 00:37:02 -0700

    Fix minor syntax error

--- onlineservices/food-service/src/main/java/com/onlineservices/household/FoodCourtApplication.java
+++ onlineservices/food-service/src/main/java/com/onlineservices/household/FoodCourtApplication.java
@@ -7,7 +7,7 @@ import io.dropwizard.setup.Environment;
 public class FoodCourtApplication extends Application<FoodCourtConfiguration> {
 
     public static void main(final String[] args) throws Exception {
-        new Food CourtApplication().run(args);
+        new FoodCourtApplication().run(args);
     }
 
     @Override
@@ -21,7 +21,7 @@ public class FoodCourtApplication extends Application<FoodCourtConfiguration> {
     }
 
     @Override
-    public void run(final Food CourtConfiguration configuration,
+    public void run(final FoodCourtConfiguration configuration,
                     final Environment environment) {
         // TODO: implement application
     }
